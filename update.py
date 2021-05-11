8eabf3a40885f43dee376b191357741021a0e58f
Eduardo Tello <dutello@gmail.com>
2021-05-11 10:52:58 -0300

    changed the first message

--- update.py
+++ update.py
@@ -23,7 +23,7 @@ def get_updates():
 
 def format_message(todays_versions):
   date = datetime.datetime.today()
-  message = str(date.month) + "/" + str(date.day) + "\n"
+  message = "Novidades do design system em" str(date.day) + "/" +  str(date.month) + "\n"
 
   for version in todays_versions:
     description = version["description"]
